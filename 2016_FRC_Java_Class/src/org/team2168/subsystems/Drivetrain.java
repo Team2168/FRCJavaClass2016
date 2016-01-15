e1ffd7d5e2a4d90f3855a17acafce2d629dd9e87
Elijah <Elijah@10.14.3.120>
2016-01-14 19:28:47 -0500

    Wrote the Subsytem and command for a Drivetrain.

---
+++ 2016_FRC_Java_Class/src/org/team2168/subsystems/Drivetrain.java
@@ -0,0 +1,83 @@
+
+package org.team2168.subsystems;
+
+import org.team2168.commands.Drivetrain.DriveWtihJoysticks;
+
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *Drivetrain Subsystem
+ */
+public class Drivetrain extends Subsystem {
+    
+	public static Drivetrain instance = null;
+	
+	private Talon left1;
+	private Talon left2;
+	private Talon left3;
+	private Talon right1;
+	private Talon right2;
+	private Talon right3;
+	
+	
+	
+	
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	
+	private Drivetrain(){
+		left1 = new Talon(0);
+		left2 = new Talon(1);
+		left3 = new Talon(2);
+		right1 = new Talon(3);
+		right2 = new Talon(4);
+		right3 = new Talon(5);
+		
+	}
+	/**
+	 * Returns the instance of the drivetrain (singelton class)
+	 * @return Drivetrain Subsystem Object
+	 */
+	public static Drivetrain getInstance(){
+		if(instance == null){
+			instance = new Drivetrain();
+		}
+		return instance;
+	}
+	
+	/**
+	 * Drives left side of Drivetrain.
+	 * @param speed value from -1 to 1.
+	 */
+	public void driveLeft(double speed){
+		left1.set(speed);
+		left2.set(speed);
+		left3.set(speed);
+	}
+	
+	/**
+	 * Drive right side of Drivetrain
+	 * @param speed value from -1 to 1.
+	 */
+	public void driveRight(double speed){
+		right1.set(speed);
+		right2.set(speed);
+		right3.set(speed);
+	}
+
+	/**
+	 * Drives both left and right sides of the Drivetrain.
+	 * @param leftSpeed speed value for left side from -1 to 1.
+	 * @param rightSpeed speed value for right side from -1 to 1.
+	 */
+	public void drive(double leftSpeed, double rightSpeed){
+		driveLeft(leftSpeed);
+		driveRight(rightSpeed);
+	}
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        setDefaultCommand(new DriveWtihJoysticks());
+    }
+}
+
